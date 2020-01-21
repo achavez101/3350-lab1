db99d8778fa582eeef33677b4ce217da9fc0c239
Student <student@lab-311-017.cs.csubak.edu>
2020-01-21 11:08:09 -0800

    my changes during lab class

--- MyScene.h
+++ MyScene.h
@@ -8,23 +8,25 @@ class MyScene : public Scene
 {
     protected:
 
+	int x = 0;
+	int y = 0;
         // Called once when the scene begins.
         virtual void init()
         {
             // Keep this call to the parent's version of init()
             Scene::init();
             // Your code here
-
+	   
 
         }
 
         // Called once every frame. 
         virtual void update()
         {
-            // Your code here
-
-
-
+	    clearBoard();
+	    // Your code here
+	    board [y][x] = 'x';
+	    x++;
 
             // Keep this call to the parent's version of update()
             Scene::update();
